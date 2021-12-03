57cea35a84d2bf20b6d50ab3c9ddc4a2af097c69
mubarakmujawar123 <mubarak.mujawar123@gmail.com>
2021-12-03 21:17:48 +0530

    1 file modified

--- src/MovieList.js
+++ src/MovieList.js
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import List from './List';
 const MovieList = () => {
   const [moviesData, setMoviesData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const fetchMovie = () => {
-    fetch('https://swapi.dev/api/films')
+    setIsLoading(true);
+    fetch('https://swapi.dev/api/film')
       .then((response) => {
+        if (!response.status.ok) {
+          throw new Error('Something went wrong !!!');
+        }
         return response.json();
       })
       .then((data) => {
@@ -17,13 +23,23 @@ const MovieList = () => {
           };
         });
         setMoviesData(transformedMovie);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
       });
   };
 
   return (
     <React.Fragment>
       <button onClick={fetchMovie}>Fetch data</button>
-      <List moviesData={moviesData}></List>
+      {!isLoading && moviesData.length > 0 && (
+        <List moviesData={moviesData}></List>
+      )}
+      {!isLoading && moviesData.length === 0 && <p>No data to show</p>}
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && error && <p>{error}</p>}
     </React.Fragment>
   );
 };
